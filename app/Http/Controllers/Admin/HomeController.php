d4453c310d80c1f184f7428732ec2f48f8ff68b7
Miguel Ernesto Herrera Labrada <meherreralab@gmail.com>
2023-08-26 16:53:45 -0400

    Bashboard

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -5,6 +5,16 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Entidad;
+use App\Models\Cliente;
+use App\Models\Producto;
+use App\Models\InformeRecepcion;
+use App\Models\OrdenDespacho;
+use App\Models\Transferencia;
+use App\Models\Conduce;
+use App\Models\Factura;
+use App\Models\User;
+
 class HomeController extends Controller
 {
     /**
@@ -14,6 +24,29 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('admin.index');
+        $entidades = Entidad::all()->count();
+        $clientes = Cliente::all()->count();
+        $productos = Producto::all()->count();
+        $informes_recepcion = InformeRecepcion::all()->count();
+        $ordenes_despacho = OrdenDespacho::all()->count();
+        $transferencias = Transferencia::all()->count();
+        $conduces = Conduce::all()->count();
+        $facturas = Factura::all()->count();
+        $usuarios = User::all()->count();
+
+        return view(
+            'admin.index',
+            compact(
+                'entidades',
+                'clientes',
+                'productos',
+                'informes_recepcion',
+                'ordenes_despacho',
+                'transferencias',
+                'conduces',
+                'facturas',
+                'usuarios'
+            )
+        );
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,11 +3,206 @@
 @section('title', config('app.name'))
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1>Módulos del Sistema</h1>
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <div class="row">
+        {{-- Entidades --}}
+        @can('admin.entidades.index')
+            <div class="col-lg-2 col-9">
+                <div class="small-box bg-primary">
+                    <div class="inner text-center">
+                        <i class="fas fa-landmark fa-2x">
+                            <strong class='pl-4'>{{ $entidades }}</strong>
+                        </i>
+
+                    </div>
+                    <a href="{{ route('entidades.index') }}" class="small-box-footer">
+                        <div class='input-group'>
+                            <div class='text-center ml-3 mt-3 mb-2'> {{ __('Entidades') }}</div>
+                            <div><i class="fas fa-arrow-circle-right ml-3 mt-3 mb-3"></i></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        @endcan
+
+        {{-- Clientes --}}
+        @can('admin.clientes.index')
+            <div class="col-lg-2 col-9">
+                <div class="small-box bg-success">
+                    <div class="inner text-center">
+                        <i class="fas fa-users fa-2x">
+                            <strong class='pl-4'>{{ $clientes }}</strong>
+                        </i>
+                    </div>
+                    <a href="{{ route('clientes.index') }}" class="small-box-footer">
+                        <div class='input-group'>
+                            <div class='text-center ml-3 mt-3 mb-2'>{{ __('Clientes') }}</div>
+                            <div><i class="fas fa-arrow-circle-right ml-4 mt-3 mb-3"></i></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        @endcan
+
+        {{-- Almacenes --}}
+        @can('admin.almacenes.index')
+            <div class="col-lg-2 col-9">
+                <div class="small-box bg-danger">
+                    <div class="inner text-center">
+                        <i class="fas fa-warehouse fa-2x">
+                            <strong class='pl-4'>{{ $clientes }}</strong>
+                        </i>
+                    </div>
+                    <a href="{{ route('almacenes.index') }}" class="small-box-footer">
+                        <div class='input-group'>
+                            <div class='text-center ml-3 mt-3 mb-2'>{{ __('Almacenes') }}</div>
+                            <div><i class="fas fa-arrow-circle-right ml-3 mt-3 mb-3"></i></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        @endcan
+
+        {{-- Productos --}}
+        @can('admin.productos.index')
+            <div class="col-lg-2 col-9">
+                <div class="small-box bg-warning">
+                    <div class="inner text-center">
+                        <i class="fas fa-boxes fa-2x">
+                            <strong class='pl-4'>{{ $productos }}</strong>
+                        </i>
+                    </div>
+                    <a href="{{ route('productos.index') }}" class="small-box-footer">
+                        <div class='input-group'>
+                            <div class='text-center ml-3 mt-3 mb-2'>{{ __('Productos') }}</div>
+                            <div><i class="fas fa-arrow-circle-right ml-3 mt-3 mb-3"></i></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        @endcan
+
+        {{-- Informes de Recpeción --}}
+        @can('admin.informes_recepcion.index')
+            <div class="col-lg-2 col-9">
+                <div class="small-box bg-black">
+                    <div class="inner text-center">
+                        <i class="fas fa-list fa-2x">
+                            <strong class='pl-4'>{{ $informes_recepcion }}</strong>
+                        </i>
+                    </div>
+                    <a href="{{ route('informes_recepcion.index') }}" class="small-box-footer">
+                        <div class='input-group'>
+                            <div class='text-center ml-2'>Informes de <br /> Recepción</div>
+                            <div><i class="fas fa-arrow-circle-right ml-3 mt-3"></i></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        @endcan
+    </div>
+
+
+    <div class="row">
+        {{-- Órdenes de despacho --}}
+        @can('admin.ordenes_despacho.index')
+            <div class="col-lg-2 col-9">
+                <div class="small-box bg-light">
+                    <div class="inner text-center">
+                        <i class="fas fa-list fa-2x">
+                            <strong class='pl-4'>{{ $ordenes_despacho }}</strong>
+                        </i>
+                    </div>
+                    <a href="{{ route('ordenes_despacho.index') }}" class="small-box-footer">
+                        <div class='input-group'>
+                            <div class='text-center ml-2'>Órdenes de <br /> Despacho</div>
+                            <div><i class="fas fa-arrow-circle-right ml-3 mt-3"></i></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        @endcan
+
+        {{-- Vales --}}
+        @can('admin.vales.index')
+            <div class="col-lg-2 col-9">
+                <div class="small-box bg-warning">
+                    <div class="inner text-center">
+                        <i class="fas fa-list fa-2x">
+                            <strong class='pl-4'>{{ $productos }}</strong>
+                        </i>
+                    </div>
+                    <a href="{{ route('vales.index') }}" class="small-box-footer">
+                        <div class='input-group'>
+                            <div class='text-center ml-3 mt-3 mb-2'>{{ __('Vales') }}</div>
+                            <div><i class="fas fa-arrow-circle-right ml-5 mt-3 mb-3"></i></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        @endcan
+
+        {{-- Transferencias --}}
+        @can('admin.transferencias.index')
+            <div class="col-lg-2 col-9">
+                <div class="small-box bg-secondary">
+                    <div class="inner text-center">
+                        <i class="fas fa-list fa-2x">
+                            <strong class='pl-4'>{{ $transferencias }}</strong>
+                        </i>
+                    </div>
+                    <a href="{{ route('transferencias.index') }}" class="small-box-footer">
+                        <div class='input-group'>
+                            <div class='text-center ml-1 mt-3 mb-2'>{{ __('Transferencias') }}</div>
+                            <div><i class="fas fa-arrow-circle-right ml-1 mt-3 mb-3"></i></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        @endcan
+
+        {{-- Conduces --}}
+        @can('admin.conduces.index')
+            <div class="col-lg-2 col-9">
+                <div class="small-box bg-info">
+                    <div class="inner text-center">
+                        <i class="fas fa-list fa-2x">
+                            <strong class='pl-4'>{{ $conduces }}</strong>
+                        </i>
+                    </div>
+                    <a href="{{ route('conduces.index') }}" class="small-box-footer">
+                        <div class='input-group'>
+                            <div class='text-center ml-4 mt-3 mb-2'>{{ __('Conduces') }}</div>
+                            <div><i class="fas fa-arrow-circle-right ml-4 mt-3 mb-3"></i></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        @endcan
+
+        {{-- Facturas --}}
+        @can('admin.facturas.index')
+            <div class="col-lg-2 col-9">
+                <div class="small-box bg-primary">
+                    <div class="inner text-center">
+                        <i class="fas fa-list fa-2x">
+                            <strong class='pl-4'>{{ $facturas }}</strong>
+                        </i>
+                    </div>
+                    <a href="{{ route('facturas.index') }}" class="small-box-footer">
+                        <div class='input-group'>
+                            <div class='text-center ml-4 mt-3 mb-2'>{{ __('Facturas') }}</div>
+                            <div><i class="fas fa-arrow-circle-right ml-4 mt-3 mb-3"></i></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        @endcan
+    </div>
+
 @stop
 
 @section('css')
@@ -15,5 +210,7 @@
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
+    <script>
+        console.log('Hi!');
+    </script>
 @stop
